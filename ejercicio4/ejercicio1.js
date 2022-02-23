d11f3753e8f7d785b24dabc9dc7ccd96c4079b9b
ijann <sugey90210@gmail.com>
2022-02-23 13:13:05 -0300

    ejercicio 4 del curso

---
+++ ejercicio4/ejercicio1.js
@@ -0,0 +1,14 @@
+/** 
+ * 
+ * En este primer ejercicio tienes que crear una cadena de texto con una frase, 
+ * por ejemplo: “El perro de mi vecina es muy molesto” y reemplazar la palabra vecina por vecino. 
+ * Por consola tienes que mostrar la frase con las palabras cambiadas.
+ * 
+*/
+
+let frase = "El perro de mi vecina es muy molesto";
+
+let nuevaFrase = frase.replace('vecina', 'vecino');
+
+console.log(frase);
+console.log(nuevaFrase);
---
+++ ejercicio4/ejercicio2.js
@@ -0,0 +1,14 @@
+/** 
+ * 
+ * Tienes que crear otra variable con una cadena de texto y 
+ * convertir todo el contenido de la cadena a mayúsculas y lo mostraréis por consola.
+ * 
+*/
+
+let cadenaTexto = 'cadena de texto dummy';
+
+let cadenaTextoUpperCase = cadenaTexto.toUpperCase();
+
+console.log(cadenaTexto);
+
+console.log(cadenaTextoUpperCase);
