0eae7faab0e74dc318e60bfef0ed94103cdf7c1d
ijann <sugey90210@gmail.com>
2022-02-23 14:43:56 -0300

    ejercicio 7 del curso - ejercicio2

--- ejercicio7/coche.js
+++ ejercicio7/coche.js
@@ -1,12 +1,36 @@
 class Coche {
-  static peso = 0;
-  static potencia = 0;
-  static marca = '';
+  static #peso = 0;
+  static #potencia = 0;
+  static #marca = '';
   
   constructor(peso, potencia, marca) {
-    this.peso = peso;
-    this.potencia = potencia;
-    this.marca = marca;
+    this.#peso = peso;
+    this.#potencia = potencia;
+    this.#marca = marca;
+  }
+
+  get peso() {
+    this.#peso;
+  }
+
+  get potencia() {
+    this.#potencia;
+  }
+
+  get marca() {
+    this.#marca;
+  }
+
+  set peso(value) {
+    this.#peso = value;
+  }
+
+  set potencia(value) {
+    this.#potencia = value;
+  }
+
+  set marca(value) {
+    this.#marca = value;
   }
 }
 
---
+++ ejercicio7/ejercicio2.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ * Modifica el archivo del ejercicio anterior para que los atributos sean privados y 
+ * tengan sus correspondientes getters y setters.
+ * 
+ */
+
